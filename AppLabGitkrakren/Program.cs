1d89cb84efb71c07baf076254b9bafc32c7c3206
Sakho <csakho@gmail.com>
2022-04-13 20:12:06 +0200

    TEST2

--- AppLabGitkrakren/Program.cs
+++ AppLabGitkrakren/Program.cs
@@ -8,4 +8,6 @@ List<Personne> personnes = new List<Personne>()
 {
     new Personne{ Id = 1, Name = "Sakho", LastName= "Cheikh"},
     new Personne{Id=3, Name = "Sow", LastName = "Maham"}
+    new Personne{ Id = 1, Name = "Sakho", LastName= "Cheikh"},
+    new Personne { Id = 2, Name = "Soumaré", LastName = "Nouma"}
 };
