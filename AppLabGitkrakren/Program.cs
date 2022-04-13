e462733a273e6325633a5b79712f663b947bae27
Sakho <csakho@gmail.com>
2022-04-13 20:16:29 +0200

    test

--- AppLabGitkrakren/Program.cs
+++ AppLabGitkrakren/Program.cs
@@ -7,7 +7,6 @@ Console.WriteLine("Hello, World!");
 List<Personne> personnes = new List<Personne>()
 {
     new Personne{ Id = 1, Name = "Sakho", LastName= "Cheikh"},
-    new Personne{Id=3, Name = "Sow", LastName = "Maham"}
-    new Personne{ Id = 1, Name = "Sakho", LastName= "Cheikh"},
+    new Personne{Id=3, Name = "Sow", LastName = "Maham"},
     new Personne { Id = 2, Name = "Soumaré", LastName = "Nouma"}
 };
